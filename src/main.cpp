73609eaa8537f489cec2018dc3fbe139f1a08a70
Shaqeeb <shaqeebmomen@yahoo.ca>
2022-10-10 14:18:55 -0400

    v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,28 @@
 
 // Conditional Compilation defines
 // #define DEBUG_FFT
-#define DEBUG_BUCKET
+// #define DEBUG_BUCKET
 
 // Software defines
 #define SAMPLE_SIZE 2048 // keep to a power of 2
-#define MAX_SCALE 1023   // value to scale the samples by
-// TODO test realistic sample scaling and adjust gain on mic breakout
+#define MAX_SCALE 45.0   // value to scale the samples by
 #define FREQ_SAMPLE 68267 // hz
-#define FREQ_LOW 500      // Frequencies below this trigger
-#define FREQ_HIGH 800
-#define FREQ_MAX 1200
+#define FREQ_LOW 200.0    // Frequencies below this trigger
+#define FREQ_HIGH 600.0
+#define FREQ_MAX 4000.0
 
+#define ANIM_COUNT 5
 // LED Count defines
-#define LED_COUNT_INNER 6
+#define LED_COUNT_INNER 5
 #define LED_COUNT_MIDDLE 4
 #define LED_COUNT_OUTER 8
 
 // Pin defines
-#define LED_PIN_INNER 2
-#define LED_PIN_MIDDLE 3
-#define LED_PIN_OUTER 4
-#define MIC_PIN 6
+#define LED_PIN_INNER 4
+#define LED_PIN_MIDDLE 8
+#define LED_PIN_OUTER 9
+#define MIC_PIN 5
+#define BTN_PIN 7
 
 enum stripName : uint8_t
 {
@@ -42,48 +43,65 @@ Adafruit_NeoPixel strips[] = {Adafruit_NeoPixel(LED_COUNT_INNER, LED_PIN_INNER, 
 
 // Data objects
 int16_t micData[SAMPLE_SIZE]; // mic signal
-int16_t bucketAvg[3];         // averages for each of the 3 zones of frequences
-uint8_t lastLED[3];
-uint32_t lastSampleTS; // timestamp of last sample capture
+float bucketAvg[3];           // averages for each of the 3 zones of frequences
+// uint8_t lastLED[3];
+// uint32_t lastSampleTS; // timestamp of last sample capture
+uint8_t animIndex = 0;
 
 // Function prototypes
 void initLEDs(void);
 void takeData(int16_t *);
-void compactData(int16_t *, int16_t *);
-void writeStrips(int);
+void compactData(int16_t *, float *);
+uint8_t calcCap(Adafruit_NeoPixel *, float);
+uint8_t calcBrightCap(float);
 void writeStaticRainbow(Adafruit_NeoPixel *, uint8_t);
-void colorWipe(Adafruit_NeoPixel *, uint32_t, int);
-void rainbow(Adafruit_NeoPixel *, int);
+void colorWipe(Adafruit_NeoPixel *, uint32_t);
+void rainbow(Adafruit_NeoPixel *, int wait);
+void writeSinglePixel(Adafruit_NeoPixel *, float, uint32_t);
+void blueLightning(Adafruit_NeoPixel *, float *);
+void fullRainbow(Adafruit_NeoPixel *, float *);
+void glowingRed(Adafruit_NeoPixel *, float *);
+void staticRed(Adafruit_NeoPixel *, float *);
+void resetLights(Adafruit_NeoPixel *, float *);
 
-uint8_t test;
+void (*anims[ANIM_COUNT])(Adafruit_NeoPixel *, float *) = {fullRainbow, blueLightning, glowingRed, staticRed, resetLights};
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  pinMode(BTN_PIN, INPUT_PULLUP);
   initLEDs();
-  test = 0;
 }
 
 // TODO control loop freq to match sampling freq (maybe two loops,1 for sampling and the other running faster for LEDs)
 void loop()
 {
-  // takeData(micData);
-  // compactData(micData, bucketAvg);
+  takeData(micData);
+  compactData(micData, bucketAvg);
+  // delay(300);
+  anims[animIndex](strips, bucketAvg);
 
-  // delay(500);
-  writeStaticRainbow(&strips[INNER], test);
-  test = (test + 1) % strips[INNER].numPixels();
-  delay(200);
+  if (!digitalRead(BTN_PIN))
+  {
+    delay(50);
+    if (!digitalRead(BTN_PIN))
+    {
+      animIndex = (animIndex + 1) % ANIM_COUNT;
+    }
+  }
+
+  // blueLightning(strips, bucketAvg);
+  // glowingRed(strips, bucketAvg);
 }
 
 void initLEDs()
 {
   // Init LED strips
-  for (uint8_t i = INNER; i != OUTER; i++)
+  for (uint8_t i = INNER; i <= OUTER; i++)
   {
     strips[i].begin();
     strips[i].show();
-    strips[i].setBrightness(50);
+    strips[i].setBrightness(130);
   }
 }
 
@@ -108,27 +126,31 @@ void takeData(int16_t *arr)
   Serial.println(sampleTime);
 #endif
 
-  //remove DC offset and gain up to 16 bits
+  //remove DC offset and add gain
   avg = avg / SAMPLE_SIZE;
   for (int i = 0; i < SAMPLE_SIZE; i++)
-    arr[i] = (arr[i] - avg);
+    arr[i] = (arr[i] - avg) * 128; //64
 
   ZeroFFT(arr, SAMPLE_SIZE);
 
 #ifdef DEBUG_FFT
   for (int i = 0; i < SAMPLE_SIZE / 2; i++)
   {
+    Serial.print(",");
     //print the frequency
     Serial.print(FFT_BIN(i, FREQ_SAMPLE, SAMPLE_SIZE));
-    Serial.print(" Hz: ");
+    // Serial.print(" Hz: ");
+    Serial.print(",");
 
     //print the corresponding FFT output
-    Serial.println(arr[i]);
+    Serial.print(arr[i]);
+    Serial.print(",");
+    Serial.println();
   }
 #endif
 }
 
-void compactData(int16_t *data, int16_t *bucket)
+void compactData(int16_t *data, float *bucket)
 {
   uint16_t count[3] = {0, 0, 0};
   // Reset bucket
@@ -138,28 +160,42 @@ void compactData(int16_t *data, int16_t *bucket)
   }
   for (uint16_t i = 0; i < SAMPLE_SIZE / 2; i++)
   {
-    float currentFreq = FFT_BIN(data[i], FREQ_SAMPLE, SAMPLE_SIZE);
-    if (currentFreq < FREQ_LOW && currentFreq > 0)
+    float currentFreq = FFT_BIN(i, FREQ_SAMPLE, SAMPLE_SIZE);
+    // Serial.println(currentFreq);
+    // Serial.println("...");
+    if (currentFreq < FREQ_LOW && currentFreq > 0.0)
     {
-      bucket[0] += data[i];
+      bucket[0] += (float)data[i];
       count[0]++;
+      // Serial.print("LOW: ");
+      // Serial.println((float)data[i]);
+      // Serial.println(bucket[0]);
+      // Serial.println(count[0]);
     }
     if (currentFreq > FREQ_LOW && currentFreq < FREQ_HIGH)
-      ;
     {
-      bucket[1] += data[i];
+      bucket[1] += (float)data[i];
       count[1]++;
+      // Serial.print("MED: ");
+      // Serial.println((float)data[i]);
+      // Serial.println(bucket[1]);
+      // Serial.println(count[1]);
     }
     if (currentFreq > FREQ_HIGH && currentFreq < FREQ_MAX)
     {
-      bucket[2] += data[i];
-      count[1]++;
+      bucket[2] += (float)data[i] * 2.0; // adding a slight gain for higher frequencies
+      count[2]++;
+      // Serial.print("HIGH: ");
+      // Serial.println((float)data[i]);
+      // Serial.println(bucket[2]);
+      // Serial.println(count[2]);
     }
+    // delay(200);
   }
 
   for (uint8_t i = 0; i < 3; i++)
   {
-    // bucket[i] /= count[i];
+    bucket[i] /= (float)count[i];
   }
 
 #ifdef DEBUG_BUCKET
@@ -173,16 +209,90 @@ void compactData(int16_t *data, int16_t *bucket)
   }
 #endif
 }
-// TODO function to scale fft buckets
 
-void writeStrips(int bucketAvg)
+uint8_t calcCap(Adafruit_NeoPixel *strip, float bucketAvg)
 {
-  // calculate the last LED to turn on for each bucket
-  // remember to only use half the data
+  uint8_t cap;
+  if (bucketAvg < MAX_SCALE)
+  {
+    cap = (float)strip->numPixels() * bucketAvg / MAX_SCALE;
+  }
+  else
+  {
+    cap = strip->numPixels();
+  }
+
+  return cap;
+}
+
+uint8_t calcBrightCap(float bucketAvg)
+{
+  return (float)255 * bucketAvg / MAX_SCALE;
 }
 
 /*******************ANIMATION FUNCTIONS**********************/
 
+void staticRed(Adafruit_NeoPixel *strips, float *buckets)
+{
+  for (uint8_t i = INNER; i <= OUTER; i++)
+  {
+    colorWipe(&strips[i], strips[i].Color(255, 0, 0));
+  }
+}
+
+void blueLightning(Adafruit_NeoPixel *strips, float *buckets)
+{
+  float scale = (buckets[0] / MAX_SCALE);
+  colorWipe(&strips[INNER], strips[INNER].Color(20 * scale, 60 * scale, 255 * scale));
+  for (uint8_t i = MIDDLE; i <= OUTER; i++)
+  {
+    writeSinglePixel(&strips[i], buckets[OUTER - i], strips[INNER].Color(20 * scale, 60 * scale, 255 * scale));
+  }
+}
+
+void fullRainbow(Adafruit_NeoPixel *strips, float *buckets)
+{
+  for (uint8_t i = INNER; i <= OUTER; i++)
+  {
+    writeStaticRainbow(&strips[i], calcCap(&strips[i], bucketAvg[OUTER - i]));
+  }
+}
+
+void glowingRed(Adafruit_NeoPixel *strips, float *buckets)
+{
+  for (uint8_t i = INNER; i <= OUTER; i++)
+  {
+    colorWipe(&strips[i], strips[INNER].Color(calcBrightCap(buckets[OUTER - i]), 0, 0));
+  }
+}
+
+void resetLights(Adafruit_NeoPixel *strips, float *buckets)
+{
+  for (uint8_t i = INNER; i <= OUTER; i++)
+  {
+    colorWipe(&strips[i], strips[i].Color(0, 0, 0));
+  }
+}
+
+/*****************LOWER LEVEL ANIMATION FUNCTIONS***********/
+void writeSinglePixel(Adafruit_NeoPixel *strip, float bucket, uint32_t color)
+{
+  uint32_t off = strip->Color(0, 0, 0);
+  for (uint8_t i = 0; i < strip->numPixels(); i++)
+  {
+    if (i != calcCap(strip, bucket))
+    {
+      strip->setPixelColor(i, off);
+    }
+    else
+    {
+      strip->setPixelColor(i, color);
+    }
+  }
+  strip->show();
+}
+
+// Writes a static rainbow to a strip that's length is based on a passed cap
 void writeStaticRainbow(Adafruit_NeoPixel *strip, uint8_t count)
 {
 
@@ -210,14 +320,13 @@ void writeStaticRainbow(Adafruit_NeoPixel *strip, uint8_t count)
   strip->show();
 }
 
-void colorWipe(Adafruit_NeoPixel *strip, uint32_t color, int wait)
+void colorWipe(Adafruit_NeoPixel *strip, uint32_t color)
 {
   for (int i = 0; i < strip->numPixels(); i++)
   {                                 // For each pixel in strip...
     strip->setPixelColor(i, color); //  Set pixel's color (in RAM)
-    strip->show();                  //  Update strip to match
-    delay(wait);                    //  Pause for a moment
   }
+  strip->show(); //  Update strip to match
 }
 
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
@@ -243,6 +352,6 @@ void rainbow(Adafruit_NeoPixel *strip, int wait)
       strip->setPixelColor(i, strip->gamma32(strip->ColorHSV(pixelHue)));
     }
     strip->show(); // Update strip with new contents
-    delay(wait);   // Pause for a moment
+    delay(wait);
   }
 }
